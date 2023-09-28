a5179cffc209b60b06f33ccd7e752a4f56958944
punykauas <punykauasecundaria@mail.com>
2023-09-28 10:08:05 -0300

    Initial commit

---
+++ cod.js
@@ -0,0 +1,25 @@
+function res(){
+    let n1=4 
+    let n2=3 
+    let n3=n1+n2 
+    return n3 } 
+    let num =res() 
+    console.log("me retornou"+(num))
+
+
+
+
+
+
+    function soma2_10(){
+        let n1=2
+        let n2=10
+        let soma = n1+n2
+        console.log(soma)
+    }
+
+    for(let i=0;i<10;1++){
+        soma2_10()
+
+
+    }
